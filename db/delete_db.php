35038a38c0d02cb39a04d1b60ec5053050041b98
Robb Lewis <me@therobb.com>
2013-03-22 15:14:55 +0000

    Delete now deletes images as well

--- db/delete_db.php
+++ db/delete_db.php
@@ -4,14 +4,23 @@
 	$resetquery = $_GET['query'];
 	
 	require('connect_db.php');
+
+	$query = "SELECT photo_url FROM products WHERE product_id = $product_id";
+
+	$result = mysqli_query($dbc, $query);
+	
+	if ($result) {
+			while ( $row = mysqli_fetch_array($result)){
+				$file = "../img/products/".$row['photo_url'];
+				unlink($file);
+		}
+	}
 	
 	$query = "DELETE FROM products WHERE product_id = $product_id";
 	
 	mysqli_query($dbc, $query);
 	
 	echo "<p class='feedback_yes'>Product deleted</p>";
-	
-	include("../db/connect_db.php");
 			
 	$query = $resetquery;
 			
