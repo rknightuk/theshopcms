4a98efa6aa66adb2e4c188eb2a7aeb5e09df236a
Robb Lewis <Robb Lewis>
2013-04-25 14:34:46 +0100

    Updated order status page

--- db/get_order_status.php
+++ db/get_order_status.php
@@ -7,15 +7,18 @@ $order_id = $_POST['ordernumber'];
 
 $query = "SELECT cust_email, delivered, delivered_date
 FROM customers, orders
-WHERE cust_email = '".$email."' AND order_id = $order_id";
+WHERE cust_email = '".$email."' AND order_id = ".$order_id."";
 
 $result = mysqli_query($dbc, $query);
 
 if (mysqli_num_rows($result) > 0){
 	while ($row = mysqli_fetch_array($result)) {
 	echo "<p><strong>Order ID: #".$order_id." Email: <em>".$email."</em></strong><p>";
-	if ($row['delivered'] = true){
+	if ($row['delivered'] == 1){
 		echo "<p>Your order was delivered ".$row['delivered_date'].".</p>";
+	}
+	else {
+		echo "<p>Your order has not been delivered yet</p>";
 	}
 
 }
